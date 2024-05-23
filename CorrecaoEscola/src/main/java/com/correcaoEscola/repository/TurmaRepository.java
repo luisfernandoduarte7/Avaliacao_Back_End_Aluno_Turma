06d65740fa1c2f3835e3b29bcd9cc48eb883ec80
luisfernandogaldino <133809489+luisfernandogaldino@users.noreply.github.com>
2024-05-23 18:23:41 -0300

    Revert "Add files via upload"

--- CorrecaoEscola/src/main/java/com/correcaoEscola/repository/TurmaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.correcaoEscola.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.correcaoEscola.entities.Turma;
-
-public interface TurmaRepository  extends JpaRepository<Turma,Long>{
-
-}
